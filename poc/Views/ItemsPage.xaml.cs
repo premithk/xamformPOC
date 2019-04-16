f5807810ff393093070b783a3fa44e82a99f1f88
Premith <premith@primarybid.com>
2019-04-16 16:15:46 +0530

    Initial commit

---
+++ poc/Views/ItemsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using poc.Models;
+using poc.Views;
+using poc.ViewModels;
+
+namespace poc.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        public ItemsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await System.Threading.Tasks.Task.Delay(250);
+                entryPOC.Focus();
+            });
+        }
+    }
+}
